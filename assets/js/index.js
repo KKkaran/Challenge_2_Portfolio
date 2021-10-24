de9d69e02f343e11551d7d2efc94e967ab84e6ba
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-10-24 00:37:01 -0400

    js file update with toggle button

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,4 @@ const navbarlinks = document.getElementsByClassName("navbar-links")[0]
 hamburger_btn.addEventListener("click", function(){
     navbarlinks.classList.toggle('active')
 })
+//this above code is to toggle b/w hamburger menu for the nav bar!!
